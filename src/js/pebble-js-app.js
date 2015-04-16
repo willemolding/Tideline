e8e51a735e96107c9b01b0d6bc0087394288fe22
willemolding <544oxwou>
2015-04-16 12:43:08 +1000

    adds javascript to print the user-app token

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,30 @@
 Pebble.addEventListener("ready",
+  
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
+
+        Pebble.getTimelineToken(
+		  function (token) {
+		    console.log('My timeline token is ' + token);
+
+		  },
+		  function (error) { 
+		    console.log('Error getting timeline token: ' + error);
+		  }
+		);
+
     }
+);
+
+
+Pebble.addEventListener('showConfiguration', function(e) {
+  // Show config page
+  Pebble.openURL('https://my-website.com/config-page.html');
+});
+
+
+Pebble.addEventListener('webviewclosed',
+  function(e) {
+    console.log('Configuration window returned: ' + e.response);
+  }
 );
