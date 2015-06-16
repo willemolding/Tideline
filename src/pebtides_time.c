929bde35fb6de631dd0fc561166f837344eaba3f
willemolding <willemolding@gmail.com>
2015-06-16 16:21:15 +1000

    adds animations and colours. Loses aplite support

--- src/pebtides_time.c
+++ src/pebtides_time.c
@@ -1,5 +1,8 @@
 #include <pebble.h>
 
+#define SCREEN_WIDTH 144
+#define SCREEN_HEIGHT 168
+
 #define MAX_TIDE_EVENTS 4
 #define MAX_NAME_LENGTH 48
 
@@ -22,11 +25,18 @@ typedef union IntByteArray
 
 // the text layers to display the info
 static Window *window;
+
+static Layer *blue_layer;
+
 static TextLayer *name_text_layer;
 static TextLayer *tide_event_text_layer;
 static TextLayer *at_text_layer;
 static TextLayer *height_text_layer;
 static TextLayer *counter_text_layer;
+
+#define tide_event_text_layer_bounds (GRect) { .origin = { LEFT_MARGIN, 40 }, .size = { SCREEN_WIDTH - LEFT_MARGIN, 50 } }
+#define at_text_layer_bounds (GRect) { .origin = { LEFT_MARGIN, 80 }, .size = { SCREEN_WIDTH - LEFT_MARGIN, 50 } }
+
 
 static IntByteArray times;
 static IntByteArray heights;
@@ -72,6 +82,74 @@ static void update_display_data() {
     text_layer_set_text(counter_text_layer,counter_text);
 
 }
+
+static Animation *create_anim_scroll_out(Layer *layer, int up) {
+  //creates an animation that shrinks a layer into either its top or bottom edge
+  // Set start and end
+  GRect from_frame = layer_get_frame(layer);
+  GRect to_frame;
+  if(up) {
+    to_frame = GRect(from_frame.origin.x, from_frame.origin.y, from_frame.size.w, 0);
+  }
+  else {
+    to_frame = GRect(from_frame.origin.x, from_frame.origin.y + from_frame.size.h, from_frame.size.w, 0);
+  }
+
+  // Create the animation
+  return (Animation*) property_animation_create_layer_frame(layer, &from_frame, &to_frame);
+}
+
+static Animation *create_anim_scoll_in(Layer *layer, GRect dest, int up) {
+  GRect from_frame;
+  GRect to_frame = dest;
+
+  if(up) {
+    from_frame = GRect(to_frame.origin.x, to_frame.origin.y + to_frame.size.h, to_frame.size.w, 0);
+  }
+  else {
+    from_frame = GRect(to_frame.origin.x, to_frame.origin.y, to_frame.size.w, 0);
+  }
+  return (Animation*) property_animation_create_layer_frame(layer, &from_frame, &to_frame);
+}
+
+void animation_started(Animation *animation, void *data) {
+   // Animation started!
+
+}
+
+void animation_stopped(Animation *animation, bool finished, void *data) {
+   update_display_data();
+}
+
+static Animation *create_anim_scroll(int down) {
+  //the combined animation for scrolling up all the fields
+
+  //first create all the scroll out animations and combine to a spawn animation
+  Animation *tide_event_text_layer_out_animation =  create_anim_scroll_out((Layer*) tide_event_text_layer, down);
+  Animation *at_text_layer_out_animation =  create_anim_scroll_out((Layer*) at_text_layer, down);
+
+  Animation *scroll_out = animation_spawn_create(tide_event_text_layer_out_animation,at_text_layer_out_animation, NULL);
+
+  // add a callback to swap the data at the end of the scoll out animation
+  // You may set handlers to listen for the start and stop events
+  animation_set_handlers(scroll_out, (AnimationHandlers) {
+    .started = (AnimationStartedHandler) animation_started,
+    .stopped = (AnimationStoppedHandler) animation_stopped,
+  }, NULL);
+
+  //same with the scoll in animations
+  Animation *tide_event_text_layer_in_animation = create_anim_scoll_in((Layer*) tide_event_text_layer, tide_event_text_layer_bounds, down);
+  Animation *at_text_layer_in_animation = create_anim_scoll_in((Layer*) at_text_layer, at_text_layer_bounds, down);
+
+  Animation *scroll_in = animation_spawn_create(tide_event_text_layer_in_animation,at_text_layer_in_animation, NULL);
+
+  //create a sequence animation from the scroll out and scroll in
+  return animation_sequence_create(scroll_out, scroll_in, NULL);
+
+}
+
+
+
 
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
@@ -124,36 +202,55 @@ static void inbox_dropped_callback(AppMessageResult reason, void *context) {
   APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
 }
 
+static void blue_layer_update_callback(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  graphics_context_set_fill_color(ctx, GColorPictonBlue);
+  graphics_fill_rect(ctx, GRect(bounds.origin.x, bounds.origin.y + SCREEN_HEIGHT/2, bounds.size.w, bounds.size.h), 0, GCornerNone);
+}
+
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
+  window_set_background_color(window, GColorBrass);
   GRect bounds = layer_get_bounds(window_layer);
+
+  //add the blue layer at the base
+  blue_layer = layer_create(bounds);
+  layer_set_update_proc(blue_layer, blue_layer_update_callback);
+  layer_add_child(window_layer, blue_layer);
+
 
   //create the name text layer
   name_text_layer = text_layer_create((GRect) { .origin = { LEFT_MARGIN, 10 }, .size = { bounds.size.w - 40, 30 } });
   text_layer_set_font(name_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_background_color(name_text_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(name_text_layer));
 
   //create the event text layer
-  tide_event_text_layer = text_layer_create((GRect) { .origin = { LEFT_MARGIN, 40 }, .size = { bounds.size.w - LEFT_MARGIN, 50 } });
+  tide_event_text_layer = text_layer_create(tide_event_text_layer_bounds);
   text_layer_set_text(tide_event_text_layer, "Getting Data");
   text_layer_set_font(tide_event_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    text_layer_set_background_color(tide_event_text_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(tide_event_text_layer));
 
   //create the at text layer
-  at_text_layer = text_layer_create((GRect) { .origin = { LEFT_MARGIN, 80 }, .size = { bounds.size.w - LEFT_MARGIN, 50 } });
+  at_text_layer = text_layer_create(at_text_layer_bounds);
   text_layer_set_font(at_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_background_color(at_text_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(at_text_layer));
 
   //create the height text layer
   height_text_layer = text_layer_create((GRect) { .origin = { LEFT_MARGIN, 120 }, .size = { bounds.size.w - LEFT_MARGIN, 50 } });
   text_layer_set_font(height_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_background_color(height_text_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(height_text_layer));
 
   //create the counter text layer
   counter_text_layer = text_layer_create((GRect) { .origin = { bounds.size.w - 30, 10 }, .size = { bounds.size.w , 30 } });
   text_layer_set_font(name_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_background_color(counter_text_layer, GColorClear);
   layer_add_child(window_layer, text_layer_get_layer(counter_text_layer));
+
 }
 
 static void window_unload(Window *window) {
@@ -163,14 +260,14 @@ static void window_unload(Window *window) {
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   if(data_index > 0 && has_data) {
     data_index -= 1;
-    update_display_data();
+    animation_schedule(create_anim_scroll(0));
   }
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   if(data_index < (n_events - 1) && has_data) {
     data_index += 1;
-    update_display_data();
+    animation_schedule(create_anim_scroll(1));
   }
 }
 
@@ -188,6 +285,7 @@ static void init(void) {
     .unload = window_unload,
   });
   const bool animated = true;
+
   window_stack_push(window, animated);
 
   app_message_register_inbox_received(inbox_received_callback);
